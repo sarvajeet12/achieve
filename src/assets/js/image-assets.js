80ce5f267f1db192e050537d43c95c56066caa36
Sarvajeet <sarvajeetshahktn@gmail.com>
2025-01-02 16:18:43 +0530

    adding tic-tac-toe card

--- src/assets/js/image-assets.js
+++ src/assets/js/image-assets.js
@@ -1,11 +1,13 @@
 // images
 import Todo from "../images/list.jpg"
 import AgeCalc from "../images/ageCalc.jpg"
+import TicTacToe from "../images/tic-tac-toe.jpg"
 
 
 
 
 export const images = {
     Todo,
-    AgeCalc
+    AgeCalc,
+    TicTacToe
 }
--- src/data/card.js
+++ src/data/card.js
@@ -13,12 +13,12 @@ export const cardList = [
         description: "An age calculator is a tool that determines a person's age based on their birthdate by calculating the difference between dates.",
         path: "https://sarva-age-calc.netlify.app/"
     },
-    // {
-    //     image: images.Todo,
-    //     name: "todo list",
-    //     description: "Create a todo list with the ability to add, remove, and mark tasks as complete",
-    //     path: "https://sarvatodo.netlify.app/"
-    // },
+    {
+        image: images.TicTacToe,
+        name: "tic-tac-toe",
+        description: "Tic Tac Toe is a simple, classic two-player game where each tries to align three X's or O's in a grid.",
+        path: "https://sarva-tic-tac-toe.netlify.app/"
+    },
     // {
     //     image: images.Todo,
     //     name: "todo list",
